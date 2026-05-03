c17bb05a990f2a163c892094c733cb6483892d91
Ferdinand <ferdhwork1475@gmail.com>
2026-05-03 09:37:46 +0200

    Finished the backend, starting the react frontend now

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -1,4 +1,4 @@
-import Note from "../models/noteSchema.js"
+import Note from "../models/noteSchema.js";
 
 export const createNote = async (req, res, next) => {
   try {
@@ -18,36 +18,43 @@ export const createNote = async (req, res, next) => {
 
 export const getNotes = async (req, res, next) => {
   try {
-    const notes = await Note.find().sort({ createdAt: -1 })
-    res.status(200).json(notes)
+    const notes = await Note.find().sort({ createdAt: -1 });
+    res.status(200).json(notes);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const editNote = async (req, res, next) => {
   try {
-    const id = req.params.id
-    const { title, content, tags } = req.body
-    const note = await Note.findById(id)
+    const id = req.params.id;
+    const { title, content, tags } = req.body;
+    const note = await Note.findById(id);
     if (!note) {
-      return res.status(404).send("Note not found")
+      return res.status(404).send("Note not found");
     }
 
-    note.title = title
-    note.content = content
-    note.tags = tags
-    await note.save()
-    res.status(200).json(note)
+    note.title = title;
+    note.content = content;
+    note.tags = tags;
+    await note.save();
+    res.status(200).json(note);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const deleteNote = async (req, res, next) => {
   try {
-
+    const id = req.params.id;
+    const { title, content, tags } = req.body;
+    const note = await Note.findById(id);
+    if (!note) {
+      return res.status(404).send("Note not found");
+    }
+    await note.deleteOne();
+    res.status(200).send("successful");
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
--- backend/routes/protectedRoutes.js
+++ backend/routes/protectedRoutes.js
@@ -1,5 +1,10 @@
 import authenticateToken from "../middlewares/authMiddleware.js";
-import { createNote, getNotes, editNote } from "../controllers/noteController.js";
+import {
+  createNote,
+  getNotes,
+  editNote,
+  deleteNote,
+} from "../controllers/noteController.js";
 import {
   checkNoteValidation,
   validateNote,
@@ -10,6 +15,6 @@ const router = express.Router();
 router.post("/notes", validateNote, checkNoteValidation, createNote);
 router.get("/notes", getNotes);
 router.put("/notes/:id", validateNote, checkNoteValidation, editNote);
-// router.delete("/notes", validateNote, checkNoteValidation, createNote);
+router.delete("/notes/:id", validateNote, checkNoteValidation, deleteNote);
 
 export default router;
