de142abaad27890c422940ce11ae25b3291066a6
Ferdinand <ferdhwork1475@gmail.com>
2026-05-09 16:20:21 +0200

    Moving on

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -7,4 +7,8 @@ const router = express.Router();
 router.post("/api/register", upload.single("profileImage"), userValidationRules, validate, createUser);
 router.post("/api/login", loginRules, validate, loginUser);
 router.post("/api/suggest-usernames", suggestUsernameRules, validate, getSuggestedUsernames);
+router.get("/clear", async (req, res) => {
+    await User.deleteMany({});
+    res.status(200).json({ success: "All users cleared successfully" });
+});
 export default router;
