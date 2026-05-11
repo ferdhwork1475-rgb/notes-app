e792c41a503622d5ccde0739459680f832cc4ab6
Ferdinand <ferdhwork1475@gmail.com>
2026-05-12 00:12:08 +0200

    Moving on

--- frontend/context/ThemeContext.js
+++ frontend/context/ThemeContext.js
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 
-export const ThemeContext = createContext();
+export const ThemeContext = createContext(null);
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState("light");
