64f13390c356e3dec4870162773ea7584634a03d
Ferdinand <ferdhwork1475@gmail.com>
2026-05-19 12:57:39 +0200

    Moving on

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -5,7 +5,17 @@ import ClipLoader from "react-spinners/ClipLoader";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+  const [data, setData] = useState(null)
   
+  const verifyUserToken = async () => {
+    try {
+      const response = await verifyUser()
+      console.log(response);
+      
+    } catch (error) {
+      throw error
+    }
+  }
 
   return (
     <AuthContext.Provider value={{ }}>
