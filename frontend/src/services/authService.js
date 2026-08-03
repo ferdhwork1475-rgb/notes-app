d856a69db6c5e5d04227214c6a58e24f676e34ec
ferdhwork1475-rgb <ferdhwork1475@gmail.com>
2026-08-03 22:59:41 +0100

    fix: /api

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -5,6 +5,7 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 const backendAPI = import.meta.env.VITE_BACKEND_API;
+console.log("Backend API URL:", backendAPI);
 
 export const signupUser = async (formData) => {
   try {
