04a48eb143833238e02b74ac1a2c914fa81af2e5
ferdhwork1475-rgb <ferdhwork1475@gmail.com>
2026-08-04 03:30:33 +0100

    fix: debugging, jwt wasn't sent to the frontend.

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,10 +1,11 @@
 import jwt from "jsonwebtoken";
 
 const protect = async (req, res, next) => {
-  const token = req.cookies.token
+  const token = req.cookies.token;
 
   if (!token) return res.status(401).json({ message: "No token provided" });
 
+  console.log("Token:", token)
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.status(403).json({ message: "Invalid token" });
     req.user = user;
