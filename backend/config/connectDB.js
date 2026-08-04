4c18e4eb5b9f792dc8abcd1dbdc774d805fb6538
ferdhwork1475-rgb <ferdhwork1475@gmail.com>
2026-08-04 05:45:30 +0100

    fix: debugging, Create Article.

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    console.log("MONGODBURL:", process.env.MONGODBURL);
     await mongoose.connect(process.env.MONGODBURL)
     console.log("Backend is connected to database")
   } catch (error) {
