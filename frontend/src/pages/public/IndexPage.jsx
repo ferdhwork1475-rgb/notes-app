7eda25a4e890f8f985b49b0bfc0cfcab322f7f3d
ferdhwork1475-rgb <ferdhwork1475@gmail.com>
2026-08-10 12:40:42 +0100

    fix: uploadPath

--- frontend/src/pages/public/IndexPage.jsx
+++ frontend/src/pages/public/IndexPage.jsx
@@ -357,7 +357,7 @@ const IndexPage = () => {
               >
                 <div className="relative h-64 overflow-hidden">
                   <img
-                    src={`${uploadPath}${story.thumbnail}`}
+                    src={`${story.thumbnail}`}
                     alt={story.title}
                     className="h-full w-full object-cover transition duration-700 group-hover:scale-105"
                   />
