1c51dc552c0dade92b557087252f1f4303db7ea8
ferdhwork1475-rgb <ferdhwork1475@gmail.com>
2026-08-26 15:55:18 +0100

    adjust: frontend view

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -3,8 +3,8 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import axios from "axios";
 
-// const backendAPI = import.meta.env.VITE_BACKEND_API;
-const backendAPI = "http://localhost:3000/api/"; // Replace with your backend API URL
+const backendAPI = import.meta.env.VITE_BACKEND_API;
+// const backendAPI = "http://localhost:3000/api/"; // Replace with your backend API URL
 
 export const signupUser = async (formData) => {
   try {
